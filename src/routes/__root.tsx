7e38b942997a786a5cf1937c10d626bc2423a2f1
movement-overdrive <96665110+movement-overdrive@users.noreply.github.com>
2025-12-18 21:51:41 +0100

    Fix toast message display

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,6 +2,7 @@ import { TanStackDevtools } from "@tanstack/react-devtools";
 import type { QueryClient } from "@tanstack/react-query";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import { Toaster } from "sonner";
 import Header from "../components/Header";
 import TanStackQueryDevtools from "../integrations/tanstack-query/devtools";
 
@@ -16,6 +17,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
 			<main className="pt-4 px-4 md:px-6 lg:px-8 w-full max-w-7xl mx-auto">
 				<Outlet />
 			</main>
+			<Toaster />
 			<TanStackDevtools
 				config={{
 					position: "bottom-right",
